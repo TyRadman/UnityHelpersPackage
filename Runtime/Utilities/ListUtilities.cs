fd37e16762199fa83798cf18869f3912072beaa8
Ty <timdesign1996@gmail.com>
2026-05-26 15:52:13 -0700

    feat: add a utility function to choose a random set from a set

--- Runtime/Utilities/ListUtilities.cs
+++ Runtime/Utilities/ListUtilities.cs
@@ -34,6 +34,50 @@ public static class ListUtilities
     }
 
     /// <summary>
+    /// Returns a list of random items from the list.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the list.</typeparam>
+    /// <param name="list">The list to select random items from.</param>
+    /// <param name="count">The number of items to return.</param>
+    /// <param name="allowRepeats">If true, items may repeat. If false (default), each item is returned at most once.</param>
+    /// <returns>A list of <paramref name="count" /> random items.</returns>
+    public static List<T> RandomItems<T>(this IList<T> list, int count, bool allowRepeats = false)
+    {
+        if (count <= 0 || list.Count == 0)
+        {
+            return new List<T>();
+        }
+
+        if (allowRepeats)
+        {
+            List<T> result = new List<T>(count);
+            for (int i = 0; i < count; i++)
+            {
+                result.Add(list[Random.Range(0, list.Count)]);
+            }
+            return result;
+        }
+
+        // No-repeat path: clamp count to list size, then partial Fisher-Yates on an index buffer.
+        int take = Mathf.Min(count, list.Count);
+        int[] indices = new int[list.Count];
+        for (int i = 0; i < indices.Length; i++)
+        {
+            indices[i] = i;
+        }
+
+        List<T> picked = new List<T>(take);
+        for (int i = 0; i < take; i++)
+        {
+            int swapWith = Random.Range(i, indices.Length);
+            (indices[i], indices[swapWith]) = (indices[swapWith], indices[i]);
+            picked.Add(list[indices[i]]);
+        }
+
+        return picked;
+    }
+
+    /// <summary>
     /// Creates a duplicate of the list.
     /// </summary>
     /// <typeparam name="T">The type of elements in the list.</typeparam>
