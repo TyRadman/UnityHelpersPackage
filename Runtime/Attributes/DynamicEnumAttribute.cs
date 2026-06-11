67ad605960ddf2e2a33e88e82061a50e5ebd601a
TyRadmanWork <tyradmanwork@gmail.com>
2026-06-11 11:48:38 -0700

    feat: add enum editor attribute

---
+++ Runtime/Attributes/DynamicEnumAttribute.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+/// <summary>
+/// Apply to an enum field to get a dropdown that lets you add/remove
+/// enum values directly from the inspector, which will edit the source file.
+/// </summary>
+public class DynamicEnumAttribute : PropertyAttribute { }
